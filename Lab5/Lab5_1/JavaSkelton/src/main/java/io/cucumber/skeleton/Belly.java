0f76c6e03b977ea328219a3ca4d4ac091f89621b
Danielar0w0 <danielar0000@gmail.com>
2022-04-18 23:13:11 +0100

    Lab5

---
+++ Lab5/Lab5_1/JavaSkelton/src/main/java/io/cucumber/skeleton/Belly.java
@@ -0,0 +1,18 @@
+package io.cucumber.skeleton;
+
+public class Belly {
+
+    public void eat(int cukes) {
+
+    }
+
+    public void wait(int hours) {
+
+    }
+
+    public void growl() {
+
+    }
+
+
+}
---
+++ Lab5/Lab5_3/WebAutomation/src/test/java/io/cucumber/CucumberTest.java
@@ -0,0 +1,16 @@
+package io.cucumber;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("io/github/bonigarcia")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.bonigarcia")
+public class CucumberTest {
+
+}
