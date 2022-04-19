bb9adc0ea3e8a7f773aa487df289ed94558746bc
Danielar0w0 <danielar0000@gmail.com>
2022-04-19 23:14:46 +0100

    Implemented RemoteAPIService

---
+++ HW1/backend/Project_Api/src/main/java/pt/ua/deti/tqs/hw1/project_api/controllers/APIController.java
@@ -0,0 +1,80 @@
+package pt.ua.deti.tqs.hw1.project_api.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.minidev.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import pt.ua.deti.tqs.hw1.project_api.models.*;
+import pt.ua.deti.tqs.hw1.project_api.services.RemoteAPIService;
+
+import java.rmi.Remote;
+import java.util.List;
+
+@PropertySource("classpath:application.properties")
+@RestController
+@RequestMapping("/api")
+public class APIController {
+
+    private RemoteAPIService service;
+
+    @Autowired
+    public APIController(RemoteAPIService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/world")
+    public CountryData[] getWorldData() {
+        CountryData[] covidData = service.getWorldData();
+        return covidData;
+    }
+
+    @GetMapping("/countries/all")
+    public CountryData[] getAllCountriesData() {
+        CountryData[] covidData = service.getAllCountriesData();
+        return covidData;
+    }
+
+    @GetMapping("/countries/{country}")
+    public CountryData[] getCountryData(@PathVariable String country) {
+        CountryData[] covidData = service.getCountryData(country);
+        return covidData;
+    }
+
+    @GetMapping("/states/{country}")
+    public StateData[] getCountryStatesData(@PathVariable String country) {
+        StateData[] covidData = service.getCountryStatesData(country);
+        return covidData;
+    }
+
+    @GetMapping("/recent/{country}")
+    public RecentData[] getRecentCountryData(@PathVariable String country) {
+        RecentData[] covidData = service.getRecentCountryData(country);
+        return covidData;
+    }
+
+    @GetMapping("/news/covid")
+    public List<News> getAllCovidNews() throws JsonProcessingException {
+        List<News> news = service.getAllCovidNews();
+        return news;
+    }
+
+    @GetMapping("/news/health")
+    public List<News> getAllHealthNews() throws JsonProcessingException {
+        List<News> news = service.getAllHealthNews();
+        return news;
+    }
+
+    @GetMapping("/news/vaccine")
+    public List<News> getAllVaccineNews() throws JsonProcessingException {
+        List<News> news = service.getAllVaccineNews();
+        return news;
+    }
+}
--- HW1/backend/Project_Api/src/main/java/pt/ua/deti/tqs/hw1/project_api/services/RemoteAPIService.java
+++ HW1/backend/Project_Api/src/main/java/pt/ua/deti/tqs/hw1/project_api/services/RemoteAPIService.java
@@ -1,31 +1,24 @@
-package pt.ua.deti.tqs.hw1.project_api.controllers;
+package pt.ua.deti.tqs.hw1.project_api.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pt.ua.deti.tqs.hw1.project_api.models.*;
 
 import java.util.List;
 
-@PropertySource("classpath:application.properties")
-@RestController
-@RequestMapping("/api")
-public class Controller {
+@Service
+public class RemoteAPIService {
 
     private RestTemplate restTemplate;
     private ObjectMapper objectMap;
 
     @Autowired
-    public Controller(RestTemplate restTemplate) {
+    public RemoteAPIService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
         this.objectMap = new ObjectMapper();
     }
@@ -44,26 +37,19 @@ public class Controller {
         return null;
     }
 
-    @GetMapping("/world")
     public CountryData[] getWorldData() {
-
         ResponseEntity<CountryData[]> response = restTemplate.getForEntity("/npm-covid-data/world", CountryData[].class);
         CountryData[] covidData = response.getBody();
-
         return covidData;
     }
 
-    @GetMapping("/countries/all")
     public CountryData[] getAllCountriesData() {
-
         ResponseEntity<CountryData[]> response = restTemplate.getForEntity("/npm-covid-data/", CountryData[].class);
         CountryData[] covidData = response.getBody();
-
         return covidData;
     }
 
-    @GetMapping("/countries/{country}")
-    public CountryData[] getCountryData(@PathVariable String country) {
+    public CountryData[] getCountryData(String country) {
 
         // Obtain iso for given country, if possible
         String iso = obtainIso(country);
@@ -81,8 +67,7 @@ public class Controller {
         return null;
     }
 
-    @GetMapping("/states/{country}")
-    public StateData[] getCountryStatesData(@PathVariable String country) {
+    public StateData[] getCountryStatesData(String country) {
 
         // Obtain iso for given country, if possible
         String iso = obtainIso(country);
@@ -96,8 +81,7 @@ public class Controller {
         return null;
     }
 
-    @GetMapping("/recent/{country}")
-    public RecentData[] getRecentCountryStatesData(@PathVariable String country) {
+    public RecentData[] getRecentCountryData(String country) {
 
         // Obtain iso for given country, if possible
         String iso = obtainIso(country);
@@ -111,30 +95,29 @@ public class Controller {
         return null;
     }
 
-    @GetMapping("/news/covid")
     public List<News> getAllCovidNews() throws JsonProcessingException {
-
         JSONObject jsonObject = restTemplate.getForObject("/news/get-coronavirus-news/0", JSONObject.class);
         String jsonNews = objectMap.writeValueAsString(jsonObject.get("news"));
 
         return objectMap.readValue(jsonNews, List.class);
     }
 
-    @GetMapping("/news/health")
     public List<News> getAllHealthNews() throws JsonProcessingException {
-
         JSONObject jsonObject = restTemplate.getForObject("/news/get-health-news/1", JSONObject.class);
         String jsonNews = objectMap.writeValueAsString(jsonObject.get("news"));
 
         return objectMap.readValue(jsonNews, List.class);
     }
 
-    @GetMapping("/news/vaccine")
     public List<News> getAllVaccineNews() throws JsonProcessingException {
-
         JSONObject jsonObject = restTemplate.getForObject("/news/get-vaccine-news/0", JSONObject.class);
         String jsonNews = objectMap.writeValueAsString(jsonObject.get("news"));
 
         return objectMap.readValue(jsonNews, List.class);
     }
+
+
+
+
+
 }
